ebf9e7c1fa87abe28d13095f5e5acc52ba8da4f1
Alijon Karimberdiev <79594266+alijon30@users.noreply.github.com>
2022-06-21 07:47:15 -0400

    Create 1381. Design a Stack With Increment Operation.py

---
+++ 1381. Design a Stack With Increment Operation.py
@@ -0,0 +1,65 @@
+Design a stack which supports the following operations.
+
+Implement the CustomStack class:
+
+CustomStack(int maxSize) Initializes the object with maxSize which is the maximum number of elements in the stack or do nothing if the stack reached the maxSize.
+void push(int x) Adds x to the top of the stack if the stack hasn't reached the maxSize.
+int pop() Pops and returns the top of stack or -1 if the stack is empty.
+void inc(int k, int val) Increments the bottom k elements of the stack by val. If there are less than k elements in the stack, just increment all the elements in the stack.
+ 
+
+Example 1:
+
+Input
+["CustomStack","push","push","pop","push","push","push","increment","increment","pop","pop","pop","pop"]
+[[3],[1],[2],[],[2],[3],[4],[5,100],[2,100],[],[],[],[]]
+Output
+[null,null,null,2,null,null,null,null,null,103,202,201,-1]
+Explanation
+CustomStack customStack = new CustomStack(3); // Stack is Empty []
+customStack.push(1);                          // stack becomes [1]
+customStack.push(2);                          // stack becomes [1, 2]
+customStack.pop();                            // return 2 --> Return top of the stack 2, stack becomes [1]
+customStack.push(2);                          // stack becomes [1, 2]
+customStack.push(3);                          // stack becomes [1, 2, 3]
+customStack.push(4);                          // stack still [1, 2, 3], Don't add another elements as size is 4
+customStack.increment(5, 100);                // stack becomes [101, 102, 103]
+customStack.increment(2, 100);                // stack becomes [201, 202, 103]
+customStack.pop();                            // return 103 --> Return top of the stack 103, stack becomes [201, 202]
+customStack.pop();                            // return 202 --> Return top of the stack 102, stack becomes [201]
+customStack.pop();                            // return 201 --> Return top of the stack 101, stack becomes []
+customStack.pop();                            // return -1 --> Stack is empty return -1.
+
+class CustomStack:
+
+    def __init__(self, maxSize: int):
+        self.maxSize = maxSize
+        self.list = []
+
+    def push(self, x: int) -> None:
+        if len(self.list) == self.maxSize:
+            return
+        else:
+            self.list.append(x)
+
+    def pop(self) -> int:
+        if self.list == []:
+            return -1
+        else:
+            return self.list.pop()
+
+    def increment(self, k: int, val: int) -> None:
+        if k > len(self.list):
+            for i in  range(len(self.list)):
+                self.list[i] += val
+        else:
+            for i in range(len(self.list[:k])):
+                self.list[i] += val
+
+
+# Your CustomStack object will be instantiated and called as such:
+# obj = CustomStack(maxSize)
+# obj.push(x)
+# param_2 = obj.pop()
+# obj.increment(k,val)
+
