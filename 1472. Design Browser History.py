c6d61450ec956ef65d0273778f1dd1d99dc125d0
Alijon Karimberdiev <79594266+alijon30@users.noreply.github.com>
2022-06-24 06:09:13 -0400

    Create 1472. Design Browser History.py

---
+++ 1472. Design Browser History.py
@@ -0,0 +1,71 @@
+You have a browser of one tab where you start on the homepage and you can visit another url, get back in the history number of steps or move forward in the history number of steps.
+
+Implement the BrowserHistory class:
+
+BrowserHistory(string homepage) Initializes the object with the homepage of the browser.
+void visit(string url) Visits url from the current page. It clears up all the forward history.
+string back(int steps) Move steps back in history. If you can only return x steps in the history and steps > x, you will return only x steps. Return the current url after moving back in history at most steps.
+string forward(int steps) Move steps forward in history. If you can only forward x steps in the history and steps > x, you will forward only x steps. Return the current url after forwarding in history at most steps.
+ 
+
+Example:
+
+Input:
+["BrowserHistory","visit","visit","visit","back","back","forward","visit","forward","back","back"]
+[["leetcode.com"],["google.com"],["facebook.com"],["youtube.com"],[1],[1],[1],["linkedin.com"],[2],[2],[7]]
+Output:
+[null,null,null,null,"facebook.com","google.com","facebook.com",null,"linkedin.com","google.com","leetcode.com"]
+
+Explanation:
+BrowserHistory browserHistory = new BrowserHistory("leetcode.com");
+browserHistory.visit("google.com");       // You are in "leetcode.com". Visit "google.com"
+browserHistory.visit("facebook.com");     // You are in "google.com". Visit "facebook.com"
+browserHistory.visit("youtube.com");      // You are in "facebook.com". Visit "youtube.com"
+browserHistory.back(1);                   // You are in "youtube.com", move back to "facebook.com" return "facebook.com"
+browserHistory.back(1);                   // You are in "facebook.com", move back to "google.com" return "google.com"
+browserHistory.forward(1);                // You are in "google.com", move forward to "facebook.com" return "facebook.com"
+browserHistory.visit("linkedin.com");     // You are in "facebook.com". Visit "linkedin.com"
+browserHistory.forward(2);                // You are in "linkedin.com", you cannot move forward any steps.
+browserHistory.back(2);                   // You are in "linkedin.com", move back two steps to "facebook.com" then to "google.com". return "google.com"
+browserHistory.back(7);                   // You are in "google.com", you can move back only one step to "leetcode.com". return "leetcode.com"
+
+
+
+
+class BrowserHistory:
+
+    def __init__(self, homepage: str):
+        self.homepage = homepage
+        self.websites = [self.homepage]
+        self.length = 0
+        self.current= 0
+    def visit(self, url: str) -> None:
+        self.websites = self.websites[:self.current+1]
+        self.homepage = url
+        self.websites.append(self.homepage)
+        self.length = self.current + 1
+        self.current = self.length
+    def back(self, steps: int) -> str:
+        if self.current - steps >= 0:
+            self.homepage = self.websites[self.current - steps]
+            self.current-= steps
+        elif steps > self.current:
+            self.homepage = self.websites[0]
+            self.current = 0
+        return self.homepage
+
+    def forward(self, steps: int) -> str:
+        if self.current + steps >= len(self.websites):
+            self.homepage = self.websites[-1]
+            self.current = self.length
+        else:
+            self.homepage = self.websites[self.current + steps]
+            self.current += steps
+        return self.homepage
+
+# Your BrowserHistory object will be instantiated and called as such:
+# obj = BrowserHistory(homepage)
+# obj.visit(url)
+# param_2 = obj.back(steps)
+# param_3 = obj.forward(steps)
+
