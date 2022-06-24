6a59b15fc09801fecd8b0a5d4fab86fe41c5b51f
Alijon Karimberdiev <79594266+alijon30@users.noreply.github.com>
2022-06-24 01:10:23 -0400

    Create 496. Next Greater Element I.py

---
+++ 496. Next Greater Element I.py
@@ -0,0 +1,46 @@
+The next greater element of some element x in an array is the first greater element that is to the right of x in the same array.
+
+You are given two distinct 0-indexed integer arrays nums1 and nums2, where nums1 is a subset of nums2.
+
+For each 0 <= i < nums1.length, find the index j such that nums1[i] == nums2[j] and determine the next greater element of nums2[j] in nums2. If there is no next greater element, then the answer for this query is -1.
+
+Return an array ans of length nums1.length such that ans[i] is the next greater element as described above.
+
+ 
+
+Example 1:
+
+Input: nums1 = [4,1,2], nums2 = [1,3,4,2]
+Output: [-1,3,-1]
+Explanation: The next greater element for each value of nums1 is as follows:
+- 4 is underlined in nums2 = [1,3,4,2]. There is no next greater element, so the answer is -1.
+- 1 is underlined in nums2 = [1,3,4,2]. The next greater element is 3.
+- 2 is underlined in nums2 = [1,3,4,2]. There is no next greater element, so the answer is -1.
+Example 2:
+
+Input: nums1 = [2,4], nums2 = [1,2,3,4]
+Output: [3,-1]
+Explanation: The next greater element for each value of nums1 is as follows:
+- 2 is underlined in nums2 = [1,2,3,4]. The next greater element is 3.
+- 4 is underlined in nums2 = [1,2,3,4]. There is no next greater element, so the answer is -1.
+
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        lst = []
+        
+        for i in nums1:
+            indeX = nums2.index(i)
+            print(f"Index {indeX}")
+            if indeX + 1 == len(nums2):
+                lst.append(-1)
+                continue
+            for j in nums2[indeX+1:]:
+                if j > i:
+                    lst.append(j)
+                    break
+            else:
+                lst.append(-1)
+        return lst
+      
+      
+      
