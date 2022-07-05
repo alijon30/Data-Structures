899d008a33f1391fe1c89e896977c46fc4144d73
Alijon Karimberdiev <79594266+alijon30@users.noreply.github.com>
2022-07-05 06:08:36 -0400

    Create 2265. Count Nodes Equal to Average of Subtree.py

---
+++ 2265. Count Nodes Equal to Average of Subtree.py
@@ -0,0 +1,49 @@
+Given the root of a binary tree, return the number of nodes where the value of the node is equal to the average of the values in its subtree.
+
+Note:
+
+The average of n elements is the sum of the n elements divided by n and rounded down to the nearest integer.
+A subtree of root is a tree consisting of root and all of its descendants.
+ 
+
+Example 1:
+
+
+Input: root = [4,8,5,0,1,null,6]
+Output: 5
+Explanation: 
+For the node with value 4: The average of its subtree is (4 + 8 + 5 + 0 + 1 + 6) / 6 = 24 / 6 = 4.
+For the node with value 5: The average of its subtree is (5 + 6) / 2 = 11 / 2 = 5.
+For the node with value 0: The average of its subtree is 0 / 1 = 0.
+For the node with value 1: The average of its subtree is 1 / 1 = 1.
+For the node with value 6: The average of its subtree is 6 / 1 = 6.
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def averageOfSubtree(self, root: Optional[TreeNode]) -> int:
+        self.ans = 0
+        
+        def PostOrderTraversal(rootNode):   
+            if not rootNode:
+                return 0, 0
+            
+            Left, leftCount = PostOrderTraversal(rootNode.left)
+            Right, rightCount = PostOrderTraversal(rootNode.right)
+            
+            summ = rootNode.val + Left + Right
+            counnt = 1 + leftCount + rightCount
+            
+            if rootNode.val == summ//counnt:
+                self.ans += 1
+            return summ, counnt
+        PostOrderTraversal(root)
+        return self.ans
+      
+      
+      
