7d823382839a0d7313564636e1b45072dc7d3d8a
Alijon Karimberdiev <79594266+alijon30@users.noreply.github.com>
2022-09-13 12:20:13 -0400

    Create 2024. Maximize the Confusion of an Exam.py

---
+++ 2024. Maximize the Confusion of an Exam.py
@@ -0,0 +1,70 @@
+A teacher is writing a test with n true/false questions, with 'T' denoting true and 'F' denoting false. He wants to confuse the students by maximizing the number of consecutive questions with the same answer (multiple trues or multiple falses in a row).
+
+You are given a string answerKey, where answerKey[i] is the original answer to the ith question. In addition, you are given an integer k, the maximum number of times you may perform the following operation:
+
+Change the answer key for any question to 'T' or 'F' (i.e., set answerKey[i] to 'T' or 'F').
+Return the maximum number of consecutive 'T's or 'F's in the answer key after performing the operation at most k times.
+
+ 
+
+Example 1:
+
+Input: answerKey = "TTFF", k = 2
+Output: 4
+Explanation: We can replace both the 'F's with 'T's to make answerKey = "TTTT".
+There are four consecutive 'T's.
+Example 2:
+
+Input: answerKey = "TFFT", k = 1
+Output: 3
+Explanation: We can replace the first 'T' with an 'F' to make answerKey = "FFFT".
+Alternatively, we can replace the second 'T' with an 'F' to make answerKey = "TFFF".
+In both cases, there are three consecutive 'F's.
+Example 3:
+
+Input: answerKey = "TTFTTFTT", k = 1
+Output: 5
+Explanation: We can replace the first 'F' to make answerKey = "TTTTTFTT"
+Alternatively, we can replace the second 'F' to make answerKey = "TTFTTTTT". 
+In both cases, there are five consecutive 'T's.
+
+class Solution:
+    def maxConsecutiveAnswers(self, answerKey: str, k: int) -> int:
+        window_start = 0
+        
+        max_length1 = 0
+        
+        max_count_Ts = 0
+        max_count_Fs = 0
+        
+        for window_end in range(len(answerKey)):
+            if answerKey[window_end] == "T":
+                max_count_Ts += 1
+                
+            
+            if window_end - window_start + 1 - max_count_Ts > k:
+                if answerKey[window_start] == "T":
+                    max_count_Ts -= 1
+                    
+                
+                window_start += 1
+                
+            max_length1 = max(max_length1, window_end - window_start + 1)
+        window_start = 0
+        for window_end in range(len(answerKey)):
+            if answerKey[window_end] == "F":
+                max_count_Fs += 1
+                
+            if window_end - window_start + 1 - max_count_Fs > k:
+                if answerKey[window_start] == "F":
+                    max_count_Fs -= 1
+                    
+                window_start += 1
+                
+            max_length1 = max(max_length1, window_end - window_start + 1)
+            
+        return max_length1
+      
+      
+      
+      
