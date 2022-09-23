cb051562122e6ea417bd973b06bf5e854197a6fb
Alijon Karimberdiev <79594266+alijon30@users.noreply.github.com>
2022-09-22 22:21:04 -0400

    Create 904. Fruit Into Baskets.py

---
+++ 904. Fruit Into Baskets.py
@@ -0,0 +1,59 @@
+You are visiting a farm that has a single row of fruit trees arranged from left to right. The trees are represented by an integer array fruits where fruits[i] is the type of fruit the ith tree produces.
+
+You want to collect as much fruit as possible. However, the owner has some strict rules that you must follow:
+
+You only have two baskets, and each basket can only hold a single type of fruit. There is no limit on the amount of fruit each basket can hold.
+Starting from any tree of your choice, you must pick exactly one fruit from every tree (including the start tree) while moving to the right. The picked fruits must fit in one of your baskets.
+Once you reach a tree with fruit that cannot fit in your baskets, you must stop.
+Given the integer array fruits, return the maximum number of fruits you can pick.
+
+ 
+
+Example 1:
+
+Input: fruits = [1,2,1]
+Output: 3
+Explanation: We can pick from all 3 trees.
+Example 2:
+
+Input: fruits = [0,1,2,2]
+Output: 3
+Explanation: We can pick from trees [1,2,2].
+If we had started at the first tree, we would only pick from trees [0,1].
+Example 3:
+
+Input: fruits = [1,2,3,2,2]
+Output: 4
+Explanation: We can pick from trees [2,3,2,2].
+If we had started at the first tree, we would only pick from trees [1,2].
+
+
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        window_start = 0
+        num_freq = {}
+        max_length = 0
+        for window_end in range(len(fruits)):
+            current = fruits[window_end]
+            
+            
+            if current not in num_freq:
+                num_freq[current] = 0
+            num_freq[current] += 1
+            
+            
+            
+            while len(num_freq) > 2:
+                
+                left = fruits[window_start]
+                num_freq[left] -= 1
+                
+                if num_freq[left] == 0:
+                    del num_freq[left]
+                
+                window_start += 1
+            max_length = max(max_length, window_end - window_start + 1)
+            
+        return max_length
+      
+      
