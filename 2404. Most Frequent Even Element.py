ae5532bd4d0cb1b7a82f5b32562272f01ca16494
Alijon Karimberdiev <79594266+alijon30@users.noreply.github.com>
2022-10-25 09:52:36 -0400

    Create 2404. Most Frequent Even Element.py

---
+++ 2404. Most Frequent Even Element.py
@@ -0,0 +1,57 @@
+Given an integer array nums, return the most frequent even element.
+
+If there is a tie, return the smallest one. If there is no such element, return -1.
+
+ 
+
+Example 1:
+
+Input: nums = [0,1,2,2,4,4,1]
+Output: 2
+Explanation:
+The even elements are 0, 2, and 4. Of these, 2 and 4 appear the most.
+We return the smallest one, which is 2.
+Example 2:
+
+Input: nums = [4,4,4,9,2,4]
+Output: 4
+Explanation: 4 is the even element appears the most.
+Example 3:
+
+Input: nums = [29,47,21,41,13,37,25,7]
+Output: -1
+Explanation: There is no even element.
+ 
+
+Constraints:
+
+1 <= nums.length <= 2000
+0 <= nums[i] <= 105
+
+
+
+class Solution:
+    def mostFrequentEven(self, nums: List[int]) -> int:
+        num_freq = {}
+        for num in nums:
+            if num % 2== 0:
+                if num not in num_freq:
+                    num_freq[num] =0
+                num_freq[num] += 1
+                
+        
+        min_val, max_freq = -1, -1
+        
+        for key, value in num_freq.items():
+            if value > max_freq:
+                max_freq = value
+                min_val = key
+                
+            elif value == max_freq:
+                if key < min_val:
+                    min_val = key
+        return min_val
+        
+        
+        
+        
